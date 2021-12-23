6883ebbcc67bffa0a365483f6c2ab01992ace104
seurij2 <juuso.seuri@aalto.fi>
2021-12-23 14:19:56 +0200

    Part 2 updated

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -3,7 +3,9 @@ const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  return request.then(response => {
+    return response.data
+  })
 }
 
 const create = newObject => {
