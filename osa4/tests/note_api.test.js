f0d953ad81e0b464ff0f639741ac7a48d3914c17
seurij2 <juuso.seuri@aalto.fi>
2022-01-05 14:45:17 +0200

    4.8 done

---
+++ osa4/tests/note_api.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+describe('when there is initially some blogs saved', () => {
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
