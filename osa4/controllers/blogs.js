e1c22b0c8580e1be2fd0d824e0f78d6ecd201ff3
seurij2 <juuso.seuri@aalto.fi>
2022-01-06 10:43:03 +0200

    4.13 done

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -24,4 +24,19 @@ blogRouter.post('/', async (request, response) => {
   response.json(savedBlog.toJSON())
 })
 
+blogRouter.get('/:id', async (request, response) => {
+  const person = await Blog.findById(request.params.id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
 module.exports = blogRouter
