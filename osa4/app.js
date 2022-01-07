eeec5a14cc5bea908edbc45f7121df9f7ec6695c
seurij2 <juuso.seuri@aalto.fi>
2022-01-07 16:30:19 +0200

    4.18 done

--- osa4/app.js
+++ osa4/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 console.log('connecting to', config.MONGODB_URI)
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
